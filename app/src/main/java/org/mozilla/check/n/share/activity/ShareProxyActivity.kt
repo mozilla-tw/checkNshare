8b6d941bad3a09e817e04bab8708b0c345c5df92
Teng-pao Yu <git@mtwtm.org>
2019-03-22 16:56:46 +0800

    Split ShareProxyActivity into ShareProxy and ShowResult

--- app/src/main/java/org/mozilla/check/n/share/activity/ShareProxyActivity.kt
+++ app/src/main/java/org/mozilla/check/n/share/activity/ShareProxyActivity.kt
@@ -4,11 +4,8 @@ import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.activity_share.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -24,14 +21,7 @@ class ShareProxyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_share)
-
-        setupView()
-        root_content.setOnTouchListener { view, event ->
-            finish()
-            true
-        }
-
+        //setContentView(R.layout.activity_share)
         when {
             intent?.action == Intent.ACTION_SEND -> {
                 if ("text/plain" == intent.type) {
@@ -50,19 +40,6 @@ class ShareProxyActivity : AppCompatActivity() {
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-        finish()
-    }
-
-    private fun setupView() {
-    }
-
-    override fun finish() {
-        super.finish()
-        overridePendingTransition(0, android.R.anim.fade_out)
-    }
-
     private fun handleTextCheck(inputText: String) {
         GlobalScope.launch {
             val id = addShare(ShareEntity(inputText))
@@ -78,19 +55,10 @@ class ShareProxyActivity : AppCompatActivity() {
 
                     override fun onChanged(shareEntity: ShareEntity) {
                         if (checked) {
-                            val resultView = addCheckResultView(shareEntity.cofactsResponse)
-                            resultView.findViewById<Button>(R.id.btn_share)
-                                .setOnClickListener(object : View.OnClickListener {
-                                    override fun onClick(view: View) {
-                                        val intent = Intent()
-                                        intent.component =
-                                            ComponentName(applicationContext, ShareEditorActivity::class.java)
-                                        intent.putExtra(ShareEntity.KEY_ID, shareEntity.id)
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                                        startActivity(intent)
-                                        finish()
-                                    }
-                                })
+                            finish()
+                            startActivity(Intent().apply {
+                                component = ComponentName(applicationContext, ShowResultActivity::class.java)
+                                putExtra(ShareEntity.KEY_ID, shareEntity.id) })
                         } else {
                             checked = true
                             checkRumor(shareEntity)
@@ -113,16 +81,6 @@ class ShareProxyActivity : AppCompatActivity() {
                 }
             }
         })
-    }
-
-    private fun addCheckResultView(response: Int): View {
-        val viewId = when (response) {
-            ShareEntity.RESPONSE_FALSE -> R.layout.check_result_false
-//            0 -> R.layout.check_result_neutral
-            else -> R.layout.check_result_true
-        }
-
-        return layoutInflater.inflate(viewId, root_content)
     }
 
     private fun handleProxySend(intent: Intent) {
---
+++ app/src/main/java/org/mozilla/check/n/share/activity/ShowResultActivity.kt
@@ -0,0 +1,64 @@
+package org.mozilla.check.n.share.activity
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_share.*
+import org.mozilla.check.n.share.MainApplication
+import org.mozilla.check.n.share.R
+import org.mozilla.check.n.share.persistence.ShareEntity
+
+class ShowResultActivity : AppCompatActivity() {
+
+    override fun onStop() {
+        super.onStop()
+        finish()
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(0, android.R.anim.fade_out)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+        root_content.setOnTouchListener { _, _ ->
+            finish()
+            true
+        }
+        val id = intent?.extras?.getLong(ShareEntity.KEY_ID) ?: return
+        val shareEntityLiveData = (application as MainApplication).database.shareDao().getShare(id)
+        shareEntityLiveData.observe(this@ShowResultActivity, Observer<ShareEntity> {
+            if (it == null) {
+                return@Observer
+            }
+            val resultView = addCheckResultView(it.cofactsResponse)
+            resultView.findViewById<Button>(R.id.btn_share)
+                .setOnClickListener {
+                    val intent = Intent()
+                    intent.component =
+                        ComponentName(applicationContext, ShareEditorActivity::class.java)
+                    intent.putExtra(ShareEntity.KEY_ID, it.id)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                    finish()
+                }
+        })
+    }
+
+
+    private fun addCheckResultView(response: Int): View {
+        val viewId = when (response) {
+            ShareEntity.RESPONSE_FALSE -> R.layout.check_result_false
+//            0 -> R.layout.check_result_neutral
+            else -> R.layout.check_result_true
+        }
+
+        return layoutInflater.inflate(viewId, root_content)
+    }
+}
