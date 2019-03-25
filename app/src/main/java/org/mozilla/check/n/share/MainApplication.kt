cdaab97aded81e192f2c2dfa88d4b8dd05c0f914
maliu <max@mxli.us>
2019-03-25 16:20:57 +0800

    Add Firebase/Crashlytics support

--- app/src/main/java/org/mozilla/check/n/share/MainApplication.kt
+++ app/src/main/java/org/mozilla/check/n/share/MainApplication.kt
@@ -5,20 +5,54 @@ import android.content.ComponentName
 import android.content.Intent
 import androidx.room.Room
 import com.apollographql.apollo.ApolloClient
+import io.fabric.sdk.android.Fabric
 import okhttp3.OkHttpClient
 import org.mozilla.check.n.share.entry.ClipService
 import org.mozilla.check.n.share.persistence.ShareDatabase
+import com.crashlytics.android.Crashlytics
+
 
 class MainApplication : Application() {
 
     lateinit var database: ShareDatabase
 
     lateinit var apolloClient: ApolloClient
-    val endpoint: String = "https://cofacts-api.g0v.tw/graphql"
+    private val endpoint: String = "https://cofacts-api.g0v.tw/graphql"
 
     override fun onCreate() {
         super.onCreate()
 
+        initCrashlytics()
+
+        initApolloClient()
+
+        initRoom()
+
+        initClipboardService()
+    }
+
+    private fun initCrashlytics() {
+        if (!BuildConfig.DEBUG) {
+            //  Collect report only when not debug
+            Fabric.with(this, Crashlytics())
+        }
+    }
+
+    private fun initClipboardService() {
+        val intent = Intent()
+        intent.component = ComponentName(this, ClipService::class.java)
+        startService(intent)
+    }
+
+    private fun initRoom() {
+        database = Room.databaseBuilder(
+            this,
+            ShareDatabase::class.java, "share.sqlite3"
+        )
+            .build()
+    }
+
+    private fun initApolloClient() {
         val okHttpClient = OkHttpClient.Builder()
             .build()
 
@@ -26,14 +60,6 @@ class MainApplication : Application() {
             .serverUrl(endpoint)
             .okHttpClient(okHttpClient)
             .build()
-
-        database = Room.databaseBuilder(this,
-            ShareDatabase::class.java, "share.sqlite3")
-            .build()
-
-        val intent = Intent()
-        intent.component = ComponentName(this, ClipService::class.java)
-        startService(intent)
     }
 
 }
