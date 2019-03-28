edfd790b23688e3901c60f52a7f62a04dcc11be5
Teng-pao Yu <git@mtwtm.org>
2019-03-28 19:01:37 +0800

    Implement "Share Anyway" flow

--- app/src/main/java/org/mozilla/check/n/share/widget/WhyAdapter.kt
+++ app/src/main/java/org/mozilla/check/n/share/widget/WhyAdapter.kt
@@ -2,6 +2,7 @@ package org.mozilla.check.n.share.widget
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,7 +10,11 @@ import org.json.JSONArray
 import org.json.JSONObject
 import org.mozilla.andext.findViewOften
 import org.mozilla.check.n.share.R
+import org.mozilla.check.n.share.navigation.IntentBuilder
 import org.mozilla.check.n.share.persistence.ShareEntity
+
+private const val VIEW_TYPE_FOOTER = 0
+private const val VIEW_TYPE_NORMAL = 1
 
 class WhyAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -19,26 +24,42 @@ class WhyAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun getItemCount(): Int {
         return if (::explanations.isInitialized) {
-            explanations.length()
+            explanations.length() + 1
         } else {
             0
         }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if (position == explanations.length()) {
+            VIEW_TYPE_FOOTER
+        } else {
+            VIEW_TYPE_NORMAL
+        }
+    }
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val title = holder.findViewOften<TextView>(R.id.why_title)
-        val details = holder.findViewOften<TextView>(R.id.why_details)
-        // Not possible if position exist
-        val item: JSONObject = explanations.getJSONObject(position)
-        title.setBackgroundColor(item.getColor())
-        title.text = item.getTitle()
-        details.text = item.optString("TEXT")
+        if (getItemViewType(position) == VIEW_TYPE_NORMAL) {
+            val title = holder.findViewOften<TextView>(R.id.why_title)
+            val details = holder.findViewOften<TextView>(R.id.why_details)
+            // Not possible if position exist
+            val item: JSONObject = explanations.getJSONObject(position)
+            title.setBackgroundColor(item.getColor())
+            title.text = item.getTitle()
+            details.text = item.optString("TEXT")
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.whyitem, parent, false)
-        val shareItemHolder = ShareItemHolder(itemView)
-        return shareItemHolder
+        if (viewType == VIEW_TYPE_NORMAL) {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.whyitem, parent, false)
+            val shareItemHolder = ShareItemHolder(itemView)
+            return shareItemHolder
+        } else {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.whyitem_footer, parent, false)
+            itemView.setOnClickListener { parent.context.startActivity(IntentBuilder.doShare(parent.context, result.id)) }
+            return ShareItemHolder(itemView)
+        }
     }
 
     fun initResult(shareEntity: ShareEntity) {
