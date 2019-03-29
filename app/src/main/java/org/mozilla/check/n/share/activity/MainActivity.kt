463c28cc2118b5f54fbcadaf362dbe383cf74e7b
Teng-pao Yu <git@mtwtm.org>
2019-03-29 09:28:42 +0800

    Add simple edittext in main page

--- app/src/main/java/org/mozilla/check/n/share/activity/MainActivity.kt
+++ app/src/main/java/org/mozilla/check/n/share/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -22,6 +23,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val check = findViewById<View>(R.id.check)
+        val checkInput = findViewById<EditText>(R.id.check_input)
+        check.setOnClickListener {startActivity(IntentBuilder.checkString(this@MainActivity, checkInput.text.toString()))}
 
         val adapter = ShareAdapter()
         list.adapter = adapter
